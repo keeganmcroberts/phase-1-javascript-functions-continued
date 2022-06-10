c04aa19029ed065ee5618774d97d6bd9f74ba5e5
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-06-09 19:12:25 -0500

    commit tree 65e85c517dd40199f260af9a953ae59ea24f8554

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`)
+    
+}
+saturdayFun("swim")
+
+
+let mondayWork = function(activity = "go to the office"){
+    return(`This Monday, I will ${activity}.`)
+}
+mondayWork("code")
+
+function wrapAdjective(param = "*"){
+    return function(word = "special"){
+        return(`You are ${param}${word}${param}!`)
+    }
+}
+wrapAdjective()()
